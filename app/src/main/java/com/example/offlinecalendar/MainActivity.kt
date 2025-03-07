9fed5ed9f73ac75d49a3c9d1de89ea908d5fa774
HarunRashid-dev <harunrashid9159@gmail.com>
2025-03-07 15:00:25 +0530

    need to check error

--- app/src/main/java/com/example/offlinecalendar/MainActivity.kt
+++ app/src/main/java/com/example/offlinecalendar/MainActivity.kt
@@ -1,23 +1,10 @@
-package com.example.offlinecalendar
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.intl.Locale
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.offlinecalendar.ui.theme.OfflineCalendarTheme
+import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.Month
+import java.time.format.TextStyle
+import java.util.Locale
 
 fun generateCalendar(year: Int, startMonth: Int, endMonth: Int): String {
-    val locale = Locale("en", "IN")
+    val locale = Locale("en", "IN") // Fix applied
     val stringBuilder = StringBuilder()
 
     for (month in startMonth..endMonth) {
@@ -28,16 +15,17 @@ fun generateCalendar(year: Int, startMonth: Int, endMonth: Int): String {
         stringBuilder.append("$monthName\n")
         stringBuilder.append("Sun Mon Tue Wed Thu Fri Sat\n")
 
-        val firstDayOfWeek = firstDayOfMonth.dayOfWeek.value % 7
+        val firstDayOfWeek = firstDayOfMonth.dayOfWeek.value % 7 // Convert to Sunday start index
         val daysInMonth = firstDayOfMonth.lengthOfMonth()
 
         // Print leading spaces
         for (i in 0 until firstDayOfWeek) {
-            stringBuilder.append("")
+            stringBuilder.append("    ")
         }
+
         // Print calendar days
         for (day in 1..daysInMonth) {
-            stringBuilder.append(String.format("%3d", day))
+            stringBuilder.append(String.format("%3d ", day))
 
             if ((day + firstDayOfWeek) % 7 == 0 || day == daysInMonth) {
                 stringBuilder.append("\n")
@@ -47,7 +35,8 @@ fun generateCalendar(year: Int, startMonth: Int, endMonth: Int): String {
     }
     return stringBuilder.toString()
 }
-fun main(){
-val calendarText = generateCalendar(2024, 3, 12)
-println(calendarText)
-    }
+
+fun main() {
+    val calendarText = generateCalendar(2024, 3, 12)
+    println(calendarText)
+}
